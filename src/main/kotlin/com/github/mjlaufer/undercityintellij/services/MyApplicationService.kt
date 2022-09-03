742a1b7742283fccf3f15f43bec95e3cd29219b1
GitHub Action <action@github.com>
2022-09-03 00:22:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/mjlaufer/undercityintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mjlaufer/undercityintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mjlaufer.undercityintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mjlaufer.undercityintellij.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/mjlaufer/undercityintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mjlaufer/undercityintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mjlaufer.undercityintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mjlaufer.undercityintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
